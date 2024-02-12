380749c64b74679a71b15ef74f03db3e0499ed6b
alizhanserkeshev <157126646+alizhanserkeshev@users.noreply.github.com>
2024-02-12 23:37:05 +0600

    Add files via upload

---
+++ python #4/task Python Math library.py
@@ -0,0 +1,35 @@
+#task 1
+import math
+
+degree = float(input("Input degree: "))
+radian = math.radians(degree)
+print("Output radian:", radian)
+
+#task 2
+
+height = float(input("Height: "))
+base1 = float(input("first value: "))
+base2 = float(input("second value: "))
+
+area = 0.5 * (base1 + base2) * height
+print("Output:", area)
+
+
+#task 3
+
+import math
+
+num_sides = int(input("num sides: "))
+side_length = float(input("length of a side: "))
+
+area = (num_sides * side_length**2) / (4 * math.tan(math.pi / num_sides))
+print("The area of the polygon is:", area)
+
+
+#task 4
+
+base_length = float(input("length of base: "))
+height_parallelogram = float(input("height of parallelogram: "))
+
+area_parallelogram = base_length * height_parallelogram
+print(" Output:", area_parallelogram)
